08a0c1c80af2a130e8c328f13219f65c54e958dd
Martin Tracey <matracey@outlook.ie>
2026-02-14 02:39:55 -0800

    feat(ui): add barrel export for ui components

---
+++ src/components/ui/Form/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Barrel export for Form components.
+ */
+
+export { FormField, type FormFieldProps } from "./FormField";
+export { FormInput, type FormInputProps } from "./FormInput";
+export { FormSelect, type FormSelectProps, type SelectOption } from "./FormSelect";
